4b625650b179b769b2a295db521aa8580477faeb
Ivan Hulko <ivan_hulko@epam.com>
2024-04-09 18:20:09 +0300

    Init commit

---
+++ lib/ruby/concepts.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative "concepts/version"
+
+module Ruby
+  module Concepts
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ spec/ruby/concepts_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Ruby::Concepts do
+  it "has a version number" do
+    expect(Ruby::Concepts::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
